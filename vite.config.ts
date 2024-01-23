771758e33933e20ac1956eb4de8dd7b668a28c30
ChuandongHuang <chuandong_huang@human-horizons.com>
2024-01-23 09:24:50 +0800

    feat：项目配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+/*
+ * @Author: ChuandongHuang chuandong_huang@human-horizons.com
+ * @Date: 2024-01-22 14:18:49
+ * @LastEditors: ChuandongHuang chuandong_huang@human-horizons.com
+ * @LastEditTime: 2024-01-22 15:48:58
+ * @Description:
+ */
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  server: {
+    host: "0.0.0.0", //解决vite use--host to expose
+    port: 8080, //配置端口
+    open: true, //配置默认打开浏览器
+  },
+});
